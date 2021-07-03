9d138a88eb8427fa672b075bf3ee0bae6c73de1f
PanderMusubi <pander@users.sourceforge.net>
2021-07-03 19:58:54 +0200

    second proposal 2.20.21

---
+++ scripts/download.sh
@@ -0,0 +1,3 @@
+cd ..
+scp zaph:database-tools/release-wordlist/* .
+mv -f basiswoorden-*.txt flexies-*.txt corrections.tsv romeinse-cijfers.txt wordparts.tsv elements
---
+++ scripts/new.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''Find new words from Dutch corpus word count.'''
+
+import re
+
+words = set()
+with open('../wordlist.txt') as content:
+    for line in content:
+        words.add(line[:-1])
+with open('../elements/wordparts.tsv') as content:
+    for line in content:
+        words.add(line[:-1].split('\t')[0])
+with open('../elements/corrections.tsv') as content:
+    for line in content:
+        words.add(line[:-1].split('\t')[0])
+
+condition = re.compile(r'^[a-záàäâåéèëêíìïîóòöôúùüûçñ-]+$')
+with open('dut.corpus.wordcount') as content, \
+    open('new.txt', 'w') as new:
+    for line in content:
+        line = line.strip()
+        if ' ' in line:
+            count, word = line.split(' ', 1)
+            if int(count) >= 1000 and condition.match(word) and word not in words:
+                new.write(f'{word}\n')
